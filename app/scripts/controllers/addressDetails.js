be1325a2f09b6ad67f2928e7a6c6f220b667e31a
Beltsazar <beltsazar.register@gmail.com>
2016-02-06 22:09:05 +0100

    Address CRUD supports street and city!

--- app/scripts/controllers/addressDetails.js
+++ app/scripts/controllers/addressDetails.js
@@ -7,14 +7,16 @@
  * # CitylistCtrl
  * Controller of the fieldserviceFeApp
  */
-angular.module('fieldserviceFeApp').controller('AddressDetails', function ($resource, $routeParams, $location, $filter, Addresses, Cities) {
+angular.module('fieldserviceFeApp').controller('AddressDetails', function ($resource, $routeParams, $location, $filter, $q, Addresses, Streets, Cities) {
 
   var ctrl = this;
 
   ctrl.address = {};
   ctrl.entities = {
-    cities : {},
-    city: {}
+    cities : [],
+    city: null,
+    streets: [],
+    street: null
   };
   ctrl.id = $routeParams.id;
 
@@ -22,6 +24,11 @@ angular.module('fieldserviceFeApp').controller('AddressDetails', function ($reso
   if(ctrl.id !== 'create') {
     Addresses.get({id: ctrl.id}).$promise.then(function (response) {
       ctrl.address = response;
+
+      // Get street entity
+      Addresses.get({id : ctrl.id, entity: 'street'}).$promise.then(function (response) {
+        ctrl.entities.street = response._links.self.href;
+      });
 
       // Get city entity
       Addresses.get({id : ctrl.id, entity: 'city'}).$promise.then(function (response) {
@@ -31,33 +38,47 @@ angular.module('fieldserviceFeApp').controller('AddressDetails', function ($reso
     });
   }
 
+  // Haal de totale lijst van straten op
+  Streets.get().$promise.then(function(result) {
+    ctrl.entities.streets = result._embedded.streets;
+  });
+
   // Haal de totale lijst van steden op
   Cities.get().$promise.then(function(result) {
     ctrl.entities.cities = result._embedded.cities;
   });
 
   // Save de bewerkte resource
-  this.saveAddress = function() {
-    Addresses.update({id : ctrl.id}, ctrl.address).$promise.then(function() {
 
-      // Save city entity
-      Addresses.updateEntity({id : ctrl.id, entity: 'city'}, ctrl.entities.city).$promise.then(function() {
+  this.saveAddress = function() {
+    Addresses.update({id : ctrl.id}, ctrl.address).$promise.then(function(response) {
+    }).then(function() {
+      return Addresses.updateEntity({id : ctrl.id, entity: 'street'}, ctrl.entities.street).$promise.then(function(response){
+      });
+    }).then(function() {
+      Addresses.updateEntity({id : ctrl.id, entity: 'city'}, ctrl.entities.city).$promise.then(function(response){
         $location.path('/addresses');
       });
-
     });
+
   };
 
   // Maak nieuwe resource
   this.createAddress = function() {
+    var generatedId = null;
+
     Addresses.add({}, ctrl.address).$promise.then(function(response) {
-
-      // Save city using newly id of newly created address
-      Addresses.updateEntity({id : $filter('id')(response._links.self.href), entity: 'city'}, ctrl.entities.city).$promise.then(function() {
-        $location.path($filter('path')(response._links.self.href));
+      generatedId = $filter('id')(response._links.self.href);
+    }).then(function() {
+      // Save street using id of newly created address
+      return Addresses.updateEntity({id : generatedId, entity: 'street'}, ctrl.entities.street).$promise.then(function() {
       });
-
+    }).then(function() {
+      Addresses.updateEntity({id : generatedId, entity: 'city'}, ctrl.entities.city).$promise.then(function() {
+        $location.path('/addresses');
+      });
     });
+
   };
 
   // Verwijder de resource
--- app/scripts/controllers/addressList.js
+++ app/scripts/controllers/addressList.js
@@ -14,6 +14,7 @@ angular.module('fieldserviceFeApp').controller('AddressList', function (Addresse
   ctrl.addresses = [];
 
   Addresses.get({projection: 'full'}).$promise.then(function(result) {
+    console.log('addresses loaded')
     ctrl.addresses = result._embedded.addresses;
   });
 
