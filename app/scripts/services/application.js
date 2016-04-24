e02a2b94e4d7d1310e43745ba13b1d40933ea2e2
Beltsazar <beltsazar.register@gmail.com>
2016-04-24 16:38:35 +0200

    hasRole

--- app/scripts/services/application.js
+++ app/scripts/services/application.js
@@ -9,13 +9,35 @@
  */
 angular.module('fieldserviceFeApp').factory('Application', function ($resource, config) {
 
-  var state = {
+  function hasRole(level) {
+    /* USER, ADMIN, ROOT */
+    if (application.account === null) {
+      return false;
+    }
+
+    if (angular.equals(level,'ROOT')) {
+      return angular.equals(application.account.role, 'ROOT');
+    }
+
+    if (angular.equals(level,'ADMIN')) {
+      console.log('hier')
+      return angular.equals(application.account.role, 'ROOT') || angular.equals(application.account.role, 'ADMIN');
+    }
+
+    if (angular.equals(level,'USER')) {
+      return angular.equals(application.account.role, 'ROOT') || angular.equals(application.account.role, 'ADMIN') || angular.equals(application.account.role, 'USER');
+    }
+
+    return false;
+  }
+
+  var application = {
     account: null,
     isAuthorized: false,
-    showLogin: false
+    showLogin: false,
+    hasRole: hasRole
   };
 
-
-  return state;
+  return application;
 
 });
