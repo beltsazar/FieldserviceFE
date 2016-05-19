ffc02e287cdc7afa4563da17d881ec18b5dc9c7f
Beltsazar <beltsazar.register@gmail.com>
2016-05-19 20:43:16 +0200

    refactored area map view

--- app/scripts/controllers/areaList.js
+++ app/scripts/controllers/areaList.js
@@ -1,3 +1,4 @@
+/* global L */
 'use strict';
 
 /**
@@ -19,22 +20,61 @@ angular.module('fieldserviceFeApp').controller('AreaList', function (Areas, Map)
    */
 
   function initializeMap () {
-    var map = new Map('AreaOverview');
+    var areaMap = new Map('AreaOverview');
 
-    angular.forEach(ctrl.areas, function (area) {
-      if(angular.isDefined(area.shape)) {
-        map.getLayer(JSON.parse(area.shape), {
-          label: area.number,
-          popup: '<a href="#/admin/areas/' + area.id + '"><b>' + area.city.name + ' ' + area.number + '</b></a>'
-        });
+    var geoJsonLayer = new L.GeoJSON(undefined, {
+      onEachFeature: function (feature, layer) {
+        var popup = '<a href="#/admin/areas/' + feature.properties.id + '"><b>' + feature.properties.city + ' ' + feature.properties.number + '</b></a>';
+        layer.bindPopup(popup);
+      },
+      pointToLayer: function (feature, latlng) {
+        var options = {
+          icon : L.divIcon({
+            className: 'area-div-icon',
+            iconSize: null,
+            html: feature.properties.number
+          })
+        };
+        return L.marker([latlng.lat, latlng.lng], options);
       }
     });
+
+    angular.forEach(ctrl.areas, function (area) {
+
+      if(angular.isDefined(area.shape)) {
+        var geoJsonObject = JSON.parse(area.shape);
+
+		  /**
+		   * Enrich Json object with area properties
+       */
+      angular.forEach(geoJsonObject.features, function (feature) {
+          feature.properties = {
+            id: area.id,
+            type: area.type,
+            number: area.number,
+            city: area.city.name
+          };
+        });
+
+        geoJsonLayer.addData(geoJsonObject);
+      }
+    });
+
+    var bounds = geoJsonLayer.getBounds();
+    if(bounds.isValid()) {
+      areaMap.map.fitBounds(bounds);
+    }
+
+    areaMap.map.addLayer(geoJsonLayer);
 
   }
 
   Areas.query({sort : 'number', projection: 'entities'}).$promise.then(function(result) {
     ctrl.areas = result;
-    initializeMap();
+
+    if (ctrl.areas.length > 0) {
+      initializeMap();
+    }
   });
 
 });
